2d226c3768718ddbe67754ec11dc28eb205a073c
LEO <kataliastriker@gmail.com>
2022-02-18 19:22:14 +0200

    ht-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const pictureGalleryItems = document.querySelector('.gallery');
+const galleryMarkup = clickImgGallery(galleryItems);
+
+pictureGalleryItems.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function clickImgGallery(elements) {
+    return elements
+        .map(({ preview, original, description }) => {
+        return `
+      <div class="gallery__item" >
+         <a class="gallery__link" 
+         href="${original}" >
+              <img class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                    />
+                </a>
+</div>
+            `;
+    })
+        .join('');
+}
+
+var gallery = new SimpleLightbox('.gallery a', { captionDelay: '250' });
